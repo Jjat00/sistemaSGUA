d27ce122b9a742d9ff05c94aaff16dd5ab7a95a9
Milton Lenis <mlenism@unal.edu.co>
2020-10-13 20:43:30 -0500

    Añadí MainController y GUI

--- src/sistemasgua/SistemaSGUA.java
+++ src/sistemasgua/SistemaSGUA.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistemasgua;
 
-import dao.UserDAOImpl;
-import model.Cliente;
-import views.GUISistemaSGUA;
-
-import java.sql.SQLException;
+import controllers.MainController;
 
 /**
  *
@@ -25,8 +16,6 @@ public class SistemaSGUA {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        Cliente cliente = new Cliente(108859745, 1, "jjat", "00", 316427787, "jjat@gmail.com");
-        System.out.println(cliente.toString());
-
+        MainController controller = new MainController();
     }
 }
